655da0587ea4fdb9f105551eaf35f2723e49d541
Yang <zhy0216@gmail.com>
2023-10-21 16:25:31 +0800

    fix ci

--- tests/integration/expression.spec.ts
+++ tests/integration/expression.spec.ts
@@ -5,7 +5,7 @@ import { describe } from "bun:test";
 describe("testExpression", () => {
   const projectRoot = import.meta.dir.split("/").slice(0, -2).join("/");
   const fixturePath = [projectRoot, "fixtures", "expression"].join(path.sep);
-  Bun.spawnSync(["mkdir", " -p /tmp${fixturePath}"]);
+  Bun.spawnSync(["mkdir", "-p", `/tmp${fixturePath}`]);
 
   testFixtures(fixturePath);
 });
--- tests/integration/statement.spec.ts
+++ tests/integration/statement.spec.ts
@@ -5,7 +5,7 @@ import { describe } from "bun:test";
 describe("testStatement", () => {
   const projectRoot = import.meta.dir.split("/").slice(0, -2).join("/");
   const fixturePath = [projectRoot, "fixtures", "statement"].join(path.sep);
-  Bun.spawnSync(["mkdir", " -p /tmp${fixturePath}"]);
+  Bun.spawnSync(["mkdir", "-p", `/tmp${fixturePath}`]);
 
   testFixtures(fixturePath);
 });
