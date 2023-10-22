86191e2e248790ddbf2576ba5021bfb5324ea755
Yang <zhy0216@gmail.com>
2023-10-22 18:46:27 +0800

    fix error

--- src/emit/expression/literal.ts
+++ src/emit/expression/literal.ts
@@ -1,8 +1,18 @@
 import { Emitter } from "../../type";
-import ts from "typescript";
+import ts, { SyntaxKind } from "typescript";
 
 export const literalEmitter: Emitter<
   ts.NumericLiteral | ts.BooleanLiteral | ts.StringLiteral
 > = (node) => ({
-  emit: () => node.getText(),
+  emit: () => {
+    if (node.kind === SyntaxKind.TrueKeyword) {
+      return "1";
+    }
+
+    if (node.kind === SyntaxKind.FalseKeyword) {
+      return "0";
+    }
+
+    return node.getText();
+  },
 });
